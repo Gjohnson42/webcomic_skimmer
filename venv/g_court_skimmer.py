562f66bd2586762fc47b94523e76e9aabfa4d4f2
Garrett <gjohnson2718@gmail.com>
2019-07-16 12:09:29 +0200

    I got it to the point where it can skim the first image, no problem. Now just to figure out how to make a loop that gets all of them, without crashing.

--- venv/g_court_skimmer.py
+++ venv/g_court_skimmer.py
@@ -1,15 +1,34 @@
 import bs4
 import requests
 import os
+import logging
+import shutil
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 def skim_g_c():
-    destination_folder = r"C:\Users\Garrett\Documents\Webcomic Skims\G_C"
+    destination_folder = r"C:\Users\Garrett\Documents\Webcomic Skims\gc"
+    download_number = 1
     curr_url = r"https://www.gunnerkrigg.com/?p=1"
     #First, create a request to the first page of the archive.
     res = requests.get(curr_url)
     res.raise_for_status()
-    print("Downloading page " +str(curr_url))
-    soup_page = bs4.BeautifulSoup(res.text, 'features="html.parser"' )
+    logging.debug("Downloading page " +str(curr_url))
+    soup_page = bs4.BeautifulSoup(res.text )
     #All right, now you have the page downloaded, and an object that contains all its information. Next, find the image.
-    comic_elem = soup_page.select()
+    comic_elem = soup_page.select("body > div.comic > img")
+    if comic_elem == []:
+        logging.debug("Evidently, that tag wasn't the right one to find that image.")
+    else:
+            curr_image_url = r'https://www.gunnerkrigg.com' + comic_elem[0].get('src')
+            logging.debug(curr_image_url)
+            # Download the image.
+            print('Downloading image %s...' % (curr_image_url))
+            res = requests.get(curr_image_url)
+            res.raise_for_status()
+            imageFile = open(destination_folder+"\\"+str(download_number),'wb')
+            for chunk in res.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+
 skim_g_c()
